abb453318cac564e582611d7b0c0a301dafd589f
marthaberner <marthaberner.mb@gmail.com>
2014-12-23 09:42:07 -0800

    rollback for student demo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def current_user
-    User.find_by(id: session[:user_id])
-  end
 
-  def authenticate_user
-    if !current_user
-      redirect_to signin_path, notice: "You must be signed in to do that!"
-    end
-  end
 
-  helper_method :current_user, :authenticate_user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  has_many :posts
   has_secure_password
   validates :email, presence: true, uniqueness: true
 end
